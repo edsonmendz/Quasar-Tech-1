ee642fb26b529fe5faa527eb1b8324f698578b64
edsonmendz <75332178+edsonmendz@users.noreply.github.com>
2025-03-20 00:13:22 -0300

    Checkbox com seus devidos números

--- Quasar-Tech/src/app/componentes/MyCheckbox.jsx
+++ Quasar-Tech/src/app/componentes/MyCheckbox.jsx
@@ -1,34 +1,47 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
-const MyCheckbox = ({number}) => {
+const MyCheckbox = ({ number }) => {
   const [checked, setChecked] = useState(false);
 
   return (
     <TouchableOpacity
-      style={[styles.checkbox, checked && styles.checked]}
+      style={[styles.checkbox, checked ? styles.checked : styles.unchecked]}
       onPress={() => setChecked(!checked)}
     >
-      {checked && <Text style={styles.checkmark}>{number}</Text>}
+      <Text style={[styles.number, checked ? styles.checkedText : styles.uncheckedText]}>
+        {number}
+      </Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   checkbox: {
-    width: 25, 
+    width: 27, // Tamanho pequeno
     height: 25,
     borderWidth: 2,
-    borderColor: '#6200ee',
+    borderRadius: 2,
     alignItems: 'center',
     justifyContent: 'center',
   },
   checked: {
     backgroundColor: '#6200ee',
+    borderColor: '#6200ee',
   },
-  checkmark: {
-    fontSize: 15,
-    color: '#fff'
+  unchecked: {
+    backgroundColor: 'transparent',
+    borderColor: '#000',
+  },
+  number: {
+    fontSize: 15, // Ajuste conforme necessário
+    fontWeight: 'bold',
+  },
+  checkedText: {
+    color: '#fff', // Número branco quando marcado
+  },
+  uncheckedText: {
+    color: '#6200ee', // Número na cor principal quando desmarcado
   },
 });
 
